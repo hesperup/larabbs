22cc619434cbb5c4a30f0af0246ce37b8b9e9b01
hesper <hesperup@gmail.com>
2020-05-21 07:39:51 +0000

    HOME page

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Route;
 // });
 
 
-Route::get('/','PagesController@root')->name('root');
+Route::get('/','TopicsController@index')->name('root');
 
 Auth::routes();
 
