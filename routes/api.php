57811078dfbfe04badea5ec3230cb4a86c7cfced
hesper <hesperup@gmail.com>
2020-05-23 00:27:19 +0000

    delete topic

--- routes/api.php
+++ routes/api.php
@@ -85,6 +85,8 @@ $api->version(
 
             $api->patch('topics/{topic}', 'TopicsController@update')
                 ->name('api.topics.update');
+            $api->delete('topics/{topic}', 'TopicsController@destroy')
+                ->name('api.topics.destroy');
         });
 
         // 游客可以访问的接口
