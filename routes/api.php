09b7dcf717eb4e7ec60a281d720d04ac477eb746
hesper <hesperup@gmail.com>
2020-05-23 01:29:37 +0000

    replies index

--- routes/api.php
+++ routes/api.php
@@ -93,6 +93,14 @@ $api->version(
             // 删除回复
             $api->delete('topics/{topic}/replies/{reply}', 'RepliesController@destroy')
                 ->name('api.topics.replies.destroy');
+
+            // 话题回复列表
+            $api->get('topics/{topic}/replies', 'RepliesController@index')
+                ->name('api.topics.replies.index');
+
+            // 某个用户的回复列表
+            $api->get('users/{user}/replies', 'RepliesController@userIndex')
+                ->name('api.users.replies.index');
         });
 
         // 游客可以访问的接口
